259d557383d208422a373d218dad58e0ede0dc4a
kashifsoofi <kashif.soofi+git@gmail.com>
2021-01-27 21:30:02 +0000

    Rust function and variable

--- tinymd/src/main.rs
+++ tinymd/src/main.rs
@@ -1,3 +1,13 @@
+fn get_version() -> u16 {
+    1000
+}
+
+fn usage() {
+    let version = get_version();
+    println!("tinymd, a markdown compiler written by Kashif");
+    println!("Version {}", version);
+}
+
 fn main() {
-    println!("Hello, world!");
+    usage();
 }
