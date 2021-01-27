5639563a6ccee171fd47434102b0cfcd46c68f68
kashifsoofi <kashif.soofi+git@gmail.com>
2021-01-27 21:19:05 +0000

    Chapter 1 Hello World with Rust

---
+++ tinymd/target/debug/deps/tinymd-422987b81392e912.d
@@ -0,0 +1,5 @@
+/home/kashif/dev/github/tinymd/tinymd/target/debug/deps/tinymd-422987b81392e912: src/main.rs
+
+/home/kashif/dev/github/tinymd/tinymd/target/debug/deps/tinymd-422987b81392e912.d: src/main.rs
+
+src/main.rs:
